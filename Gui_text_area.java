470e72ee700ff40a867856dd10b0e6a08986d0ac
LeoneJM-CS <188081003+LeoneJM-CS@users.noreply.github.com>
2025-04-16 09:11:23 -0500

    Add files via upload

---
+++ Gui_text_area.java
@@ -0,0 +1,39 @@
+package ClassWork;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.text.*;
+public class Gui_text_area extends JFrame implements ActionListener, ChangeListener {
+//	private JTextArea output;                  this is the start of another code
+//	private JButton calcButton;
+//	private JFormattedTextField savings;
+//	private JFormattedTextField interest;
+//	private JFormattedTextField years;
+	private JTextArea maxOutput;
+	private JButton maxButton;
+	private JButton minButton;
+	private JLabel max;
+	private JTable vals;
+	private JFormattedTextField maxField;
+	private String[] column;
+	private String[][] table_vals;
+	
+	public Gui_text_area() {
+		
+	}
+	
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+	public static void main(String [] args) {
+		
+	}
+}
